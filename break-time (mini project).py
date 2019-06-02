655281559bc69d5de816564f3cdf17340597bef5
pranav anand <42691222+pranavanand24@users.noreply.github.com>
2019-06-02 23:53:17 +0530

    Update break-time (mini project).py

--- break-time (mini project).py
+++ break-time (mini project).py
@@ -11,7 +11,7 @@ total_breaks = 3
 break_count = 0
 print ("this program started on "+time.ctime())
 while ( break_count < total_breaks):
-    time.sleep(10)
+    time.sleep(3600)
     webbrowser.open("https://youtu.be/96n2gRDPWyM")
     break_count = break_count + 1 
      
